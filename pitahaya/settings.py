04a5024dc0887032dcd8451020c2b0ef1a30cf38
walejandro <arguellowalter@gmail.com>
2019-02-23 13:26:07 -0500

    database set

--- pitahaya/settings.py
+++ pitahaya/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'pitahaya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pitahayadb',
+        'USER': 'postgres',
+        'PASSWORD': 'bellavista2019',
+        'HOST': 'localhost'
     }
 }
 
