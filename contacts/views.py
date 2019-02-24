01a6d7a8621f4c17d4f21f548a63811f5b97b9e9
walejandro <arguellowalter@gmail.com>
2019-02-23 19:49:29 -0500

    contact form submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        contact = Contact(name=name, email=email, message=message)
+
+        contact.save()
+
+        messages.success(request, 'El formulario ha sido enviado, te escribiremos pronto')
+        return redirect('/#contacto')
