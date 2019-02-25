6a3eedcc5e55cae36b107e1fc9d38199441384b9
walejandro <arguellowalter@gmail.com>
2019-02-24 21:51:58 -0500

    pymes page added

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('#hero', views.hero, name='hero'),
+    path('#agencia', views.agencia, name='agencia'),
+    path('#servicios', views.servicios, name='servicios'),
+    path('#portfolio', views.portfolio, name='portfolio'),
+    path('#contacto', views.contacto, name='contacto'),
+    path('pymes/', views.pymes, name='pymes'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def pymes(request):
+    return render(request, 'pages/pymes.html')
+
+def hero(request):
+    return redirect('/#hero')
+
+def agencia(request):
+    return redirect('/#agencia')
+
+def servicios(request):
+    return redirect('/#servicios')
+
+def portfolio(request):
+    return redirect('/#portfolio')
+
+def contacto(request):
+    return redirect('/#contacto')
